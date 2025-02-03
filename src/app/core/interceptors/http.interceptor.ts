85bac4d10a83297fd7ad9b1ce6400e74ac58eb3f
aayushjain-pageupsoft <ajain@pageupsoft.com>
2025-02-03 12:00:25 +0530

    completion of client and buyer seller

--- src/app/core/interceptors/http.interceptor.ts
+++ src/app/core/interceptors/http.interceptor.ts
@@ -1,5 +1,25 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
-export const httpInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
-};
+@Injectable()
+export class HttpTokenInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+      const headersConfig:any = {
+          'Accept': 'application/json'
+      };
+      const idToken = localStorage.getItem("token");
+
+      if (idToken) {
+          headersConfig['Authorization'] = idToken;
+          let request = req.clone({
+              setHeaders: headersConfig
+          });
+          return next.handle(request);
+      }
+      else {
+          return next.handle(req)
+      }
+  }
+}
